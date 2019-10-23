863d82b537d008d71bcdd5f132e1052479da8af5
PSY <p.s.84@mail.ru>
2019-10-23 23:03:52 +0300

    add task 2

--- Math_Date_JSON.js
+++ Math_Date_JSON.js
@@ -1,6 +1,7 @@
 'use strict';
 
 /*----- Задание № 1 -----*/
+
 console.log(`/*----- Задание № 1 -----*/`);
 
 let positions = [
@@ -53,3 +54,29 @@ printLotCalc(positions[2], 1);
 printLotCalc(positions[0], 2);
 printLotCalc(positions[0], 9);
 printLotCalc(positions[3], 9);
+
+/*----- Задание № 2 -----*/
+
+console.log(`/*----- Задание № 2 -----*/`);
+
+const deferedPayments = [];
+
+function deferPay(provider, summShipment, dateShipment) {
+  let datePay = new Date(dateShipment);
+  datePay.setDate(datePay.getDate() + provider.deferPeriod);
+
+  deferedPayments.push({
+    producer: provider,
+    amount: summShipment,
+    paymentDate: datePay
+  })
+}
+
+deferPay(positions[0].producer, 7200, new Date(2031, 4 - 1, 10));
+deferPay(positions[1].producer, 152589, new Date(2033, 5 - 1, 1));
+deferPay(positions[2].producer, 98410, new Date(2035, 6 - 1, 21));
+
+deferedPayments.forEach((element) =>
+  console.log(`${element.paymentDate.getDate()}.${element.paymentDate.getMonth() + 1}.${element.paymentDate.getFullYear()}: ${element.producer.name}, сумма ${element.amount} Q;`)
+)
+
